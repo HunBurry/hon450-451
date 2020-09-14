9494d6dfcc14064391b38fbfe5ebe1e90f47e5e4
Hunter Berry <htberry99@gmail.com>
2020-09-14 12:27:21 -0400

    Update xgboost_testing_phase.py

--- worksIP/xgboost_testing_phase.py
+++ worksIP/xgboost_testing_phase.py
@@ -9,20 +9,20 @@ import warnings
 import numpy as np;
 warnings.filterwarnings("ignore")
 
+topics = { 'animals' : ["lions", "boas", "elephants", "dillos"] ,
+'cars' : ["tesla", "audi", "toyota", "bmw"] }
+
+Acomments = [ 'lions and boas are good',
+'elephants are bad',
+'dillos are terrible',
+'i love tesla' ]
+
+Bcomments = [ 'boas are nice', 
+'dillos are great, but tesla stinks',
+'audi and toyota are my favorite' ]
+
 def testing():
     #############################################################
-
-    topics = { 'animals' : ["lions", "boas", "elephants", "dillos"] ,
-    'cars' : ["tesla", "audi", "toyota", "bmw"] }
-
-    Acomments = [ 'lions and boas are good',
-    'elephants are bad',
-    'dillos are terrible',
-    'i love tesla' ]
-
-    Bcomments = [ 'boas are nice', 
-    'dillos are great, but tesla stinks',
-    'audi and toyota are my favorite' ]
 
     data = [ item for item in [ (cmt, 'A') for cmt in Acomments ] * 10 ] \
     + [ item for item in [ (cmt, 'B') for cmt in Bcomments]]
@@ -45,7 +45,7 @@ def testing():
 
     #############################################################
 
-    bow_vectorizer = CountVectorizer(max_df=0.90, min_df=2, stop_words='english')
+    bow_vectorizer = CountVectorizer(stop_words='english')
     bagOfWords = bow_vectorizer.fit_transform(train['parsed_text'])
 
     x_train = bagOfWords.todense();
@@ -77,18 +77,7 @@ def testing():
 
     overall2 = np.asarray(overall);
 
-    #dataFrameHold = {"bag": x_train, "sents": overall}
-
-    #hold = pd.DataFrame(data=dataFrameHold)
-
-    #sprint(hold)
-
-    print(overall2.shape)
-    print(x_train.shape)
-    print(overall2.ndim)
-    print(x_train.ndim)
-
-    x_train = np.append(x_train, overall2);
+    x_train = np.append(x_train, overall2, axis=1);
 
     model_bow = XGBClassifier(random_state=9,learning_rate=0.9)
     model_bow.fit(x_train, y_train)
@@ -97,24 +86,47 @@ def testing():
 
 def main():
     models = testing();
-    '''
     xgb = models[0]
-    vecorizer = models[1]
+    vectorizer = models[1]
 
-    userInput = input("Give me a tweet to test:")
+    userInput = input("Give me a tweet to test: ")
 
     tokenized_tweet = userInput.split();
     ps = SnowballStemmer("english")
     tokenized_tweet = [ps.stem(i) for i in tokenized_tweet]
 
     final = ' '.join(tokenized_tweet)
-    bagOfWords = vecorizer.fit_transform([final])
+    bagOfWords = vectorizer.fit_transform([final])
 
     finalBoW = bagOfWords.todense();
+    nlp = absa.load();
+    
+    aspectsArr = topics.keys()
+    results = nlp(userInput, aspects=aspectsArr)
+    mySents = [];
+    for key in aspectsArr:
+        haveFound = False;
+        for term in topics[key]:
+            if term in userInput and not haveFound:
+                if results[key].sentiment == absa.Sentiment.negative:
+                    mySents.append(1)
+                elif results[key].sentiment == absa.Sentiment.positive:
+                    mySents.append(2);
+                haveFound = True;
+        if not haveFound:
+            mySents.append(0);
 
-    prediction = xgb.predict(finalBoW)
-    '''
+    print(finalBoW)
+    print(mySents)
 
+    mySents = np.asarray(mySents);
+
+    print(mySents);
+    
+    newInfo = np.append(finalBoW, mySents, axis=1)
+
+    prediction = xgb.predict(newInfo)
+    print(prediction)
 
 if __name__ == "__main__":
     main();
