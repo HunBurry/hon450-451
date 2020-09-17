408a6cc00e33f30630512eddbcb9d95dbaeda62c
Hunter Berry <htberry99@gmail.com>
2020-09-17 00:28:43 -0400

    updates

--- worksIP/xgboost_testing_phase.py
+++ worksIP/xgboost_testing_phase.py
@@ -7,62 +7,104 @@ from nltk import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 import warnings
 import numpy as np;
+from os import path, remove;
 warnings.filterwarnings("ignore")
 
-topics = { 'animals' : ["lions", "boas", "elephants", "dillos"] ,
-'cars' : ["tesla", "audi", "toyota", "bmw"] }
+########################################################################
 
-Acomments = [ 'lions and boas are good',
-'elephants are bad',
-'dillos are terrible',
-'i love tesla' ]
+def createTrain(topics, classificationA, classificationB, writeToText):
+    '''
+    Parameters:
+        topics: 
+            Type = Dict
+            Contains all possible topics to run ABSA on.
+        classificationA:
+            Type = Array
+            Type-A comments to reproduce.
+        classificationB:
+            Type = Array
+            Type-B comments to reproduce.         
+        writeToText
+            Type = Boolean   
+    Creates training data and saves it to a CSV file for future use. 
+    '''
+    data = [ item for item in [ (cmt, 'A') for cmt in classificationA] * 10 ] \
+        + [ item for item in [ (cmt, 'B') for cmt in classificationB] * 10 ]
 
-Bcomments = [ 'boas are nice', 
-'dillos are great, but tesla stinks',
-'audi and toyota are my favorite' ]
+    data = pd.DataFrame(data, columns=['text', 'type'])
 
-def testing():
-    #############################################################
+    if writeToText:
+        data.to_csv('data.csv', header=None, index=None, sep=' ', mode='a')
 
-    data = [ item for item in [ (cmt, 'A') for cmt in Acomments ] * 10 ] \
-    + [ item for item in [ (cmt, 'B') for cmt in Bcomments]]
+    return data;
 
-    #############################################################
+###########################################################################
 
-    train = pd.DataFrame(data, columns=['text', 'type'])
+def tokenize(dataframe):
+    '''
+    Parameters
+        dataframe
+            Type = pd.DataFrame
+            Contains all training data.
+    Tokenizes and stems text to reduce variability of words.
+    '''
+    dataframe['parsed_text'] = dataframe['text'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>2]))
 
-    train['parsed_text'] = train['text'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>2]))
-    #remove verbs? yay/nay?
-
-    tokenized_tweet = train['parsed_text'].apply(lambda x: x.split())
+    tokenized_tweet = dataframe['parsed_text'].apply(lambda x: x.split())
     ps = SnowballStemmer("english")
     tokenized_tweet = tokenized_tweet.apply(lambda x: [ps.stem(i) for i in x])
 
     for i in range(len(tokenized_tweet)):
         tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
 
-    train['parsed_text'] = tokenized_tweet;
+    dataframe['parsed_text'] = tokenized_tweet;
+    
+    return dataframe;
 
-    #############################################################
+###########################################################################
 
+def vectorize(dataframe):
+    '''
+    Parameters:
+        dataframe
+            Type = pd.DataFrame
+            Contains all training data.
+    Converts all text into a bag of words. 
+    '''
     bow_vectorizer = CountVectorizer(stop_words='english')
-    bagOfWords = bow_vectorizer.fit_transform(train['parsed_text'])
+    bagOfWords = bow_vectorizer.fit_transform(dataframe['text'])
 
     x_train = bagOfWords.todense();
-    y_train = train['type']
+    y_train = dataframe['type'];
 
-    #############################################################
+    return x_train, y_train, bow_vectorizer;
 
-    aspectsArr = topics.keys()
+###########################################################################
+
+def aspectify(topics, dataframe, writeToFile):
+    '''
+    Parameters:
+        topics:
+            Type = Dict
+            Contains all topics and keywords.
+        dataframe:
+            type = pandas.DataFrame
+            Contains all text for a given dataset.
+        writeToFile: 
+            Type = Boolean
+            If true, write to file, no return statement. 
+            If false, return the ABSA results in array form.
+    Runs ABSA on all texts to find sentiments. Uses keywords to determine overall sentiment for class. 
+    '''
+    aspectsArray = topics.keys();
     nlp = absa.load();
-    for row in range(10):
-        print("")
-    overall = [];
-    for row in range(len(x_train)):
-        rowIQ = train['text'][row];
-        results = nlp(rowIQ, aspects=aspectsArr)
+    overall = []
+
+    for row in range(len(dataframe)):
+        rowIQ = dataframe['text'][row];
+        results = nlp(rowIQ, aspects=aspectsArray)
         mySents = [];
-        for key in aspectsArr:
+        for key in aspectsArray:
             haveFound = False;
             for term in topics[key]:
                 if term in rowIQ and not haveFound:
@@ -75,59 +117,117 @@ def testing():
                 mySents.append(0);
         overall.append(mySents)
 
-    overall2 = np.asarray(overall);
+    overall = np.asarray(overall);
 
-    x_train = np.append(x_train, overall2, axis=1);
+    if writeToFile:
+        np.savetxt('sentiments.txt', overall)
+    return overall;
 
+###########################################################################
+
+def aspectifyV2(topics, dataframe, writeToFile):
+    '''
+    Parameters:
+        topics:
+            Type = Dict
+            Contains all topics and keywords.
+        dataframe:
+            type = pandas.DataFrame
+            Contains all text for a given dataset.
+        writeToFile: 
+            Type = Boolean
+            If true, write to file, no return statement. 
+            If false, return the ABSA results in array form.
+    Runs ABSA on all texts to find sentiments. Uses keywords to determine sentiment per indivudal keyword. 
+    '''
+    aspectsArray = topics.keys();
+    nlp = absa.load();
+    overall = []
+
+    for row in range(len(dataframe)):
+        rowIQ = dataframe['text'][row];
+        results = nlp(rowIQ, aspects=aspectsArray)
+        mySents = [];
+        for key in aspectsArray:
+            for term in topics[key]:
+                if term in rowIQ:
+                    if results[key].sentiment == absa.Sentiment.negative:
+                        mySents.append(1)
+                    elif results[key].sentiment == absa.Sentiment.positive:
+                        mySents.append(2);
+                    haveFound = True;
+                else:
+                    mySents.append(0);
+        overall.append(mySents)
+
+    overall = np.asarray(overall);
+
+    if writeToFile:
+        np.savetxt('sentiments.txt', overall)
+    return overall;
+
+###########################################################################
+
+def createClassifier(bagOfWords, sentiments, y_train):
+    x_train = np.append(bagOfWords, sentiments, axis=1);
+    
     model_bow = XGBClassifier(random_state=9,learning_rate=0.9)
     model_bow.fit(x_train, y_train)
 
-    return [model_bow, bow_vectorizer]
+    return model_bow;
+
+###########################################################################
+
+def removeData():
+    '''
+    Removes data files.
+    '''
+    if path.exists("bagOfWords.txt"):
+        remove("bagOfWords.txt")
+    if path.exists("sentiments.txt"):
+        remove("sentiments.txt")
+
+###########################################################################
 
 def main():
-    models = testing();
-    xgb = models[0]
-    vectorizer = models[1]
+    topics = { 'animals' : ["lions", "boas", "elephants", "dillos"] ,
+        'cars' : ["tesla", "audi", "toyota", "bmw"] }
 
-    userInput = input("Give me a tweet to test: ")
+    Acomments = [ 'lions and boas are good',
+        'elephants are bad',
+        'dillos are terrible',
+        'i love tesla' ]
 
-    tokenized_tweet = userInput.split();
-    ps = SnowballStemmer("english")
-    tokenized_tweet = [ps.stem(i) for i in tokenized_tweet]
-
-    final = ' '.join(tokenized_tweet)
-    bagOfWords = vectorizer.fit_transform([final])
-
-    finalBoW = bagOfWords.todense();
-    nlp = absa.load();
+    Bcomments = [ 'boas are nice', 
+        'elephants are amazing', 
+        'lions are cool',
+        'dillos are great, but tesla stinks',
+        'audi and toyota are my favorite' ]
     
-    aspectsArr = topics.keys()
-    results = nlp(userInput, aspects=aspectsArr)
-    mySents = [];
-    for key in aspectsArr:
-        haveFound = False;
-        for term in topics[key]:
-            if term in userInput and not haveFound:
-                if results[key].sentiment == absa.Sentiment.negative:
-                    mySents.append(1)
-                elif results[key].sentiment == absa.Sentiment.positive:
-                    mySents.append(2);
-                haveFound = True;
-        if not haveFound:
-            mySents.append(0);
+    if path.exists("text_data.txt") and path.exists("sentiments.txt") and path.exists("y_train.txt"):
+        data = pd.read_csv('text_data.csv', sep=' ', header=None, );
+        data.columns = ['text', 'type'];
+        sentiments = np.genfromtxt("sentiments.txt");
+    else:
+        data = createTrain(topics, Acomments, Bcomments, True);
+        sentiments = aspectify(topics, data, True);
+    bagOfWords, y_train, vectorizer = vectorize(data);
 
-    print(finalBoW)
-    print(mySents)
+    xgb_classifier = createClassifier(bagOfWords, sentiments, y_train);
 
-    mySents = np.asarray(mySents);
-
-    print(mySents);
+    ########################################################
     
-    newInfo = np.append(finalBoW, mySents, axis=1)
+    userInput = input("Give me a tweet to test (use enter or 'F' to close): ")
+    while (userInput != 'F' and userInput != ""):
 
-    prediction = xgb.predict(newInfo)
-    print(prediction)
+        bagOfWordsUser = vectorizer.transform([userInput])
+        sentimentsUser = np.asmatrix(aspectify(topics, pd.DataFrame({"text": [userInput]}), False));
+        totalInformation = np.append(bagOfWordsUser, sentimentsUser, axis=1); #error here
+        results = xgb_classifier.predict(totalInformation)
+
+        print("Prediction: " + results);
+
+        userInput = input("Give me a tweet to test (use enter or 'F' to close): ")
 
 if __name__ == "__main__":
     main();
-
