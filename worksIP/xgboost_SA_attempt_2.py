74c1340f6c2a901acc53076bb574b203b378d1c8
Hunter Berry <htberry99@gmail.com>
2020-09-21 20:24:50 -0400

    new testing file for real data, small changes to testing only file w/h fake data

--- worksIP/xgboost_SA_attempt_2.py
+++ worksIP/xgboost_SA_attempt_2.py
@@ -33,7 +33,7 @@ def createTrain(topics, classificationA, classificationB, writeToText):
     data = [ item for item in [ (cmt, 'A') for cmt in classificationA] * 10 ] \
         + [ item for item in [ (cmt, 'B') for cmt in classificationB] * 10 ]
 
-    data = pd.DataFrame(data, columns=['text', 'type'])
+    data = pd.DataFrame(data, columns=['tweet', 'party'])
 
     if writeToText:
         data.to_csv('data.csv', header=None, index=None, sep=' ', mode='a')
@@ -50,7 +50,7 @@ def tokenize(dataframe):
             Contains all training data.
     Tokenizes and stems text to reduce variability of words.
     '''
-    dataframe['parsed_text'] = dataframe['text'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>2]))
+    dataframe['parsed_text'] = dataframe['tweet'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>2]))
 
     tokenized_tweet = dataframe['parsed_text'].apply(lambda x: x.split())
     ps = SnowballStemmer("english")
@@ -74,10 +74,13 @@ def vectorize(dataframe):
     Converts all text into a bag of words. 
     '''
     bow_vectorizer = CountVectorizer(stop_words='english')
-    bagOfWords = bow_vectorizer.fit_transform(dataframe['text'])
+    if 'parsed_text' in dataframe.columns:
+        bagOfWords = bow_vectorizer.fit_transform(dataframe['parsed_text'])
+    else:
+        bagOfWords = bow_vectorizer.fit_transform(dataframe['tweet'])
 
     x_train = bagOfWords.todense();
-    y_train = dataframe['type'];
+    y_train = dataframe['party'];
 
     return x_train, y_train, bow_vectorizer;
 
@@ -92,10 +95,13 @@ def tfidf(dataframe):
     Converts all text into term frequency–inverse document frequency (TFIDF). 
     '''
     tfidf = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = tfidf.fit_transform(dataframe['text'])
+    if 'parsed_text' in dataframe.columns:
+        tfidf_matrix = tfidf.fit_transform(dataframe['parsed_text'])
+    else:
+        tfidf_matrix = tfidf.fit_transform(dataframe['tweet'])
 
     x_train = tfidf_matrix.todense();
-    y_train = dataframe['type'];
+    y_train = dataframe['party'];
 
     return x_train, y_train, tfidf;
 
@@ -123,7 +129,7 @@ def aspectify(topics, dataframe, writeToFile):
     overall = []
 
     for row in range(len(dataframe)):
-        rowIQ = dataframe['text'][row];
+        rowIQ = dataframe['tweet'][row];
         results = nlp(rowIQ, aspects=aspectsArray)
         mySents = [];
         for key in aspectsArray:
@@ -170,7 +176,7 @@ def aspectifyV2(topics, dataframe, writeToFile):
     overall = []
 
     for row in range(len(dataframe)):
-        rowIQ = dataframe['text'][row];
+        rowIQ = dataframe['tweet'][row];
         results = nlp(rowIQ, aspects=aspectsArray)
         mySents = [];
         for term in aspectsArray:
@@ -248,27 +254,109 @@ def removeData():
 
 ###########################################################################
 
+def test_and_predictionsV1(classifier, topics, vectorizer):
+    '''
+    Parameters:
+        classifier:
+            Type: XGBClassifer or LogisticRegressor
+            Classification system.
+        topics: 
+            Type = Dict
+            Contains all topics and keywords.
+        vectorizer:
+            Type = TFIDF or Count Vectorizer (Bag of Wordss)
+            Vectorization system for word frequency. 
+    Gets individual rows, parses individually, then computes prediction on each one. Overall prediction is most frequent prediction.
+    '''
+    data = pd.read_csv('../data/data12_02_2020_11-32.csv')
+    data = data.drop(data.columns[[0, 1, 3, 4, 6]], axis=1)
+    data = data.dropna();
+    data.columns = ['tweet', 'party'] 
+    #data = tokenize(data);
+
+    resultsOverall = []
+    for number in range(5):
+        row = data.iloc[[number]];
+        bagOfWords = vectorizer.transform([row['tweet']]);
+        bagOfWords = bagOfWords.todense()
+        sentiments = np.asmatrix(aspectifyV2(topics, pd.DataFrame({"tweet": [row['tweet']]}), False));
+        totalInformation = np.append(bagOfWords, sentiments, axis=1);
+        results = classifier.predict(totalInformation);
+        resultsOverall.append(results);
+    prediction = max(set(resultsOverall), key = resultsOverall.count);
+
+    return prediction;
+
+###########################################################################
+
+def test_and_predictionsV2(classifier, topics, vectorizer):
+    '''
+    Parameters:
+        classifier:
+            Type: XGBClassifer or LogisticRegressor
+            Classification system.
+        topics: 
+            Type = Dict
+            Contains all topics and keywords.
+        vectorizer:
+            Type = TFIDF or Count Vectorizer (Bag of Wordss)
+            Vectorization system for word frequency. 
+    Get multiple rows at once, parse them all, use that for prediction. 
+    '''
+    data = pd.read_csv('../data/data12_02_2020_11-32.csv')
+    data = data.drop(data.columns[[0, 1, 3, 4, 6]], axis=1)
+    data = data.dropna();
+    data.columns = ['tweet', 'party'] 
+    #data = tokenize(data);
+
+    data = data.head(5);
+    bagOfWords = vectorizer.transform(data['tweet']); #if tokenize, change to parsed_tweet
+    sentiments = np.asmatrix(aspectifyV2(topics, data, True))
+    totalInformation = np.append(bagOfWords, sentiments, axis=1);
+    results = classifier.predict(totalInformation);
+
+    return results;
+
+###########################################################################
+
 def main():
-    topics = { 'animals' : ["lions", "boas", "elephants", "dillos"] ,
-        'cars' : ["tesla", "audi", "toyota", "bmw"] }
+    topics = {
+        'economics': ['consumption', 'commerce', 'economics', 'economic', 'trade', 'gdp', 'China', 'investment' +
+            'stock market', 'stocks', 'stock', 'goods', 'financial', 'fiscal', 'economical', 'profitable' +
+            'economy', 'efficent', 'finance', 'monetary', 'management', 'economist', 'macroeconomics' + 
+            'microeconomics', 'protectionism', 'resources', 'real value', 'nominal value', 'capital', 'markets'],
+        'police': ['blm', 'defund', 'police', 'militiarization', 'police officer', 'sheriff', 'crime' +
+            'fatal', 'shooting', 'abuse of power', 'line of duty', 'protect', 'protecting', 'patrol',
+            'patrolling', 'law enforcement', 'riot', 'looting', 'arrest', 'racism', 'law', 'black lives matter'],
+        'foreign_policy': ['imperialism', 'occupation', 'un', 'united nations', 'united', 'hrc' +
+            'who', 'free trade', 'anarchy', 'nationalism', 'foreign', 'china', 'russia', 'cuba' +
+            'multinational', 'regional', 'trade', 'international', 'commerce', 'alien', 'refugee' + 
+            'border', 'ambassador', 'israel', 'pakistan', 'terrorism'],
+        "immigration": ['alien', 'wall', 'emigration', 'immigration', 'migration', 'illegal alien' + 
+            'naturalization', 'visa', 'citizenship', 'refugee', 'welfare', 'family reunification', 'border' + 
+            'immigrants', 'enforcement', 'seperation', 'asylum', 'sanctuary city', 'sancuary cities'],
+        "president": ['trump', 'president', 'genius', 'smart', 'incompatent', 'idiot', 'leadership' +
+            'cabinet', 'election', 'biden', 'elect', 'harris', 'joe biden', 'donald trump', 'government' +
+            'corrupt', 'russia', 'head of state', 'presidency'],
+        "military": ['military', 'armed forces', 'air force', 'coast guard', 'national guard', 'army' +
+            'navy', 'marines', 'combat', 'forces', 'invasion', 'occupation', 'overseas', 'over seas' +
+            'foreign policy', 'defense', 'intelligence', 'military intelligence', 'militaristic', 'militia' +
+            'peacekeeping', 'occupy', 'regiment', 'noncombatant', 'naval'],
+        "abortion": ['abortion', 'birth control', 'contraceptives', 'condoms', 'abortion laws', 'feticide' +
+            'abortion clinic', 'pro-choice', 'pro choice', 'prochoice', 'abortion pill', 'trimester' +
+            'first trimester', 'planned parenthood']
+    }
 
-    Acomments = [ 'lions and boas are good',
-        'elephants are bad',
-        'dillos are terrible',
-        'i love tesla' ]
+    data = pd.read_csv('../data/data12_04_2020_22-58.csv')
+    data = data.drop(data.columns[[0, 1, 3, 4, 6]], axis=1)
+    data = data.dropna();
+    data.columns = ['tweet', 'party']
 
-    Bcomments = [ 'boas are nice', 
-        'elephants are amazing', 
-        'lions are cool',
-        'dillos are great, but tesla stinks',
-        'audi and toyota are my favorite' ]
+    #data = tokenize(data);
     
-    if path.exists("data.csv") and path.exists("sentiments.txt"):
-        data = pd.read_csv('data.csv', sep=' ', header=None);
-        data.columns = ['text', 'type'];
+    if path.exists("sentiments.txt"):
         sentiments = np.genfromtxt("sentiments.txt");
     else:
-        data = createTrain(topics, Acomments, Bcomments, True);
         sentiments = aspectifyV2(topics, data, True);
     bagOfWords, y_train, vectorizer = vectorize(data);
     #tfidf_matrix, y_train, vectorizer = tfidf(data);
@@ -276,22 +364,10 @@ def main():
     xgb_classifier = createXGBClassifier(bagOfWords, sentiments, y_train);
     #xgb_classifier = createXGBClassifier(tfidf_matrix, sentiments, y_train);
     #logistic_regressor = createLogisticRegressor(bagOfWords, sentiments, y_train);
+    #logistic_regressor = createLogisticRegressor(tfidf_matrix, sentiments, y_train);
     
-    ########################################################
-    
-    userInput = input("Give me a tweet to test (use enter or 'F' to close): ")
-    while (userInput != 'F' and userInput != ""):
-
-        bagOfWordsUser = vectorizer.transform([userInput])
-        bagOfWordsUser = bagOfWordsUser.todense()
-        sentimentsUser = np.asmatrix(aspectifyV2(topics, pd.DataFrame({"text": [userInput]}), False));
-        totalInformation = np.append(bagOfWordsUser, sentimentsUser, axis=1); #error here
-        results = xgb_classifier.predict(totalInformation)
-        #results = logistic_regressor.predict(totalInformation)
-
-        print("Prediction: " + results);
-
-        userInput = input("Give me a tweet to test (use enter or 'F' to close): ")
+    #test_and_predictionsV1(xgb_classifier, topics, vectorizer);
+    #test_and_predictionsV2(xgb_classifier, topics, vectorizer);
 
 if __name__ == "__main__":
     main();
