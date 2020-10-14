cf139594022453c2548655b2ea445e7cd27252d9
Hunter Berry <htberry99@gmail.com>
2020-10-14 12:34:07 -0400

    Create cache.bat

---
+++ worksIP/cache.bat
@@ -0,0 +1,6 @@
+@echo off
+start "C:\ProgramData\Anaconda3\python.exe" "C:\hunter.berry\Documents\GitHub\hon450-451\worksIP\endlessSentimentCacher.py"
+:loop
+sleep 60
+start "C:\ProgramData\Anaconda3\python.exe" "C:\hunter.berry\Documents\GitHub\hon450-451\worksIP\endlessSentimentCacher.py"
+goto loop
