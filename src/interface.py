e0cb2cb17bc1890b55f9950b5055b250a18aa976
Hunter Berry <htberry99@gmail.com>
2021-01-07 22:21:41 -0500

    Interface patches.

--- src/interface.py
+++ src/interface.py
@@ -9,6 +9,7 @@ import sentimentCacher
 import pandas as pd
 import numpy as np
 import os
+import contextlib
 import pathlib
 
 def single_tweet_analysis(tweet):
@@ -21,25 +22,27 @@ def single_tweet_analysis(tweet):
     '''
     parent_dir = os.path.dirname(os.getcwd()).replace("\\", '/')
 
-    if len(os.listdir(parent_dir + '/data/user_models')) == 3:
-        loaded_vectorizer_model = pickle.load(open(parent_dir + '/data/user_models/vectorizer_model', 'rb'))
-        loaded_xgb_model = pickle.load(open(parent_dir + '/data/user_models/xgboost_model', 'rb'))
-        loaded_log_reg_model = pickle.load(open(parent_dir + '/data/user_models/log_reg_model', 'rb'))
-    else:
-        loaded_vectorizer_model = pickle.load(open(parent_dir + '/data/premade_models/vectorizer_model', 'rb'))
-        loaded_xgb_model = pickle.load(open(parent_dir + '/data/premade_models/xgboost_model', 'rb'))
-        loaded_log_reg_model = pickle.load(open(parent_dir + '/data/premade_models/log_reg_model', 'rb'))
+    with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+        
+        if len(os.listdir(parent_dir + '/data/user_models')) == 3:
+            loaded_vectorizer_model = pickle.load(open(parent_dir + '/data/user_models/vectorizer_model', 'rb'))
+            loaded_xgb_model = pickle.load(open(parent_dir + '/data/user_models/xgboost_model', 'rb'))
+            loaded_log_reg_model = pickle.load(open(parent_dir + '/data/user_models/log_reg_model', 'rb'))
+        else:
+            loaded_vectorizer_model = pickle.load(open(parent_dir + '/data/premade_models/vectorizer_model', 'rb'))
+            loaded_xgb_model = pickle.load(open(parent_dir + '/data/premade_models/xgboost_model', 'rb'))
+            loaded_log_reg_model = pickle.load(open(parent_dir + '/data/premade_models/log_reg_model', 'rb'))
 
-    data = pd.DataFrame([{'tweet': tweet}]);
-    bagOfWords = loaded_vectorizer_model.transform(data['tweet'])
-    bagOfWords = bagOfWords.todense();
-    sentiments = sentimentCacher.single_row_sents(tweet).to_numpy();
-    totalInformation = np.append(bagOfWords, sentiments, axis=1);
-    xgb_prediction = loaded_xgb_model.predict(totalInformation);
-    log_reg_predicition = loaded_log_reg_model.predict(totalInformation);
+        data = pd.DataFrame([{'tweet': tweet}]);
+        bagOfWords = loaded_vectorizer_model.transform(data['tweet'])
+        bagOfWords = bagOfWords.todense();
+        sentiments = sentimentCacher.single_row_sents(tweet).to_numpy();
+        totalInformation = np.append(bagOfWords, sentiments, axis=1);
+        xgb_prediction = loaded_xgb_model.predict(totalInformation);
+        log_reg_prediction = loaded_log_reg_model.predict(totalInformation);
 
     print("The XGBoost model predicts that the tweet is " + xgb_prediction + ".")
-    print("The XGBoost model predicts that the tweet is " + log_reg_prediction + ".")
+    print("The Logistic Regression model predicts that the tweet is " + log_reg_prediction + ".")
 
 def single_user_analysis(username):
     '''
@@ -75,7 +78,7 @@ def single_user_analysis(username):
     for rowNum in range(5):
         print("For tweet #" + str(rowNum) + ", the XGBoost predicited " + xgb_prediction[rowNum] + " and the Logistic Regressor predicted " + log_reg_predicition[rowNum] + '.')
 
-async def main():
+def main():
     '''
     Interface for project. Follow prompts to complete process. 
     '''
@@ -86,31 +89,32 @@ async def main():
             input2 = input("Are you sure you would like to do this? Pipeline processes may take upwards of 12 hours (Y/N). ")
             if input2.lower() == 'y':
                 print("Starting pipeline...")
-                filename = await twitterAPI.main();
+                filename = twitterAPI.main();
                 print('Starting sentiment analysis...')
-                await sentimentCacher.beginCache(filename);
+                sentimentCacher.beginCache(filename);
                 print("Sentiment analysis completed...");
                 input6 = input("Would you like to 1) use the project's default testing data, or 2) use your own (1/2)? ")
                 if (input6 == '1'):
                     print("Starting training/testing process.")
-                    await hybrid_SA.main(filename, None);
+                    hybrid_SA.main(filename, None);
                 else:
                     input7 = input("What is the filename of the testing data? Please put just the filename, path is not needed. ")
                     print("Starting training/testing process.")
-                    await hybrid_SA.main(filename, input7);
+                    hybrid_SA.main(filename, input7);
                 print("Pipeline completed, and models have been trained and stored in the user_models folder.")
         elif input1 == '2':
             input3 = input("Would you like to 1) run analysis on a single tweet/text, or 2) pull from a live feed (1/2)? ")
             if input3 == '1':
                 input4 = input("Please enter the text of the tweet. ")
-                await single_tweet_analysis(input4);
+                single_tweet_analysis(input4);
             elif input3 == '2':
                 input5 = input("Please enter the username of the user. ")
-                await single_user_analysis(input5);
+                single_user_analysis(input5);
             else:
                 print("Invalid response.")
         else:
             print("Invalid response.")
+        print("----------------------------------------------------------------------------------------------")
 
 if __name__ == '__main__':
     main();
